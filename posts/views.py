617dab8da786a4162bec69fced988323b88599dc
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-01-17 19:58:59 +0300

    UserViewSet modern to ModelViewSet

--- posts/views.py
+++ posts/views.py
@@ -38,41 +38,7 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class UserViewSet(viewsets.ViewSet):
-    def create(self, request):
-        queryset = request.data
-        serializer = UserSerializer(data=queryset)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        if not (request.user.is_staff or request.user == user):
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def list(self, request):
-        queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def partial_update(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        if not (request.user.is_staff or request.user == user):
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
